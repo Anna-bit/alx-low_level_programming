b97e8af2d6caa6aa2af8bff072e603aa2e99fea7
AnneMusau <amusau4@gmail.com>
2023-04-20 12:20:51 -0700

    Task 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-   int i
-   char c
-   long int li
-   long long int lli
-   float f
+	int i;
+        char c;
+        long int li;
+        long long int lli;
+        float f;
 
 printf("Size of a char: %d byte(s)\n", sizeof(c));
 printf("Size of an int: %d byte(s)\n", sizeof(i));
