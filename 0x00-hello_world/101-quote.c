44a1c5c201bd468f4578d2128cd96793f0f01615
AnneMusau <amusau4@gmail.com>
2023-04-20 15:08:57 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 /**
  * main - Entry point
  *
- * Return: 0 always (Success)
+ * Return: 1 always (Success)
  */
 
 int main(void)
 {
         write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (0);
+        return (1);
 }
