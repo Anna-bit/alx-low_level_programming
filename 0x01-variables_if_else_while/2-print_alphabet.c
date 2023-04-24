bbb34c95ed0ef1e03343944e5eeffb65f23ca81b
AnneMusau <amusau4@gmail.com>
2023-04-24 14:02:17 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+         	putchar(a);
+	}
+
+        putchar('\n');
+
+        return(0);
+}
