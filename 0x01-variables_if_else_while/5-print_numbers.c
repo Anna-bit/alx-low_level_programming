21f381bdcd6fe28b5b50bf3de3e60ef60aa4cd81
AnneMusau <amusau4@gmail.com>
2023-04-24 15:39:27 -0700

    5-print_numbers.c amend

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
-/**
- * main - Entry Point 
- *
- * Return : Always 0 Success
- */
-int main(void)
+int main()
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	while (i < 10)
 	{
-		putchar(i);
+            putchar(i + '0');
+	    i++;
 	}
+	
 	putchar('\n');
+
 	return(0);
 }
