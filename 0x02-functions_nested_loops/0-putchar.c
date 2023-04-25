e8f6a105e051dc35eb59b69e430277828d7bb4d7
AnneMusau <amusau4@gmail.com>
2023-04-25 13:15:54 -0700

    Amend 0-putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,11 @@
 
 
 /**
- * main - Entry Point
+ *main - Entry Point
  *
- *Return - Always (0)
- */
-int main(void)
+ *return - Always (0)
+ **/
+int main()
 {
 _putchar('_');
 _putchar('p');
@@ -16,6 +16,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('\n');
+_putchar ('\n');
 return(0);
 }
