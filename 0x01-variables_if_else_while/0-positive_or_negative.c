12468ebc39f0f86caba6c0335d176413f1658ef4
AnneMusau <amusau4@gmail.com>
2023-04-27 11:59:26 -0700

    0-positive_or_negative.c task edit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,16 +16,15 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-printf("%d is positive");
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero");
+printf("%d is zero\n", n);
 }
 else
 {
-printf("%d is negative");
+printf("%d is negative\n", n);
 }
-putchar('\n');
 return (0);
 }
