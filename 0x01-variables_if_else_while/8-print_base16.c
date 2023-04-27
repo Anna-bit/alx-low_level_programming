b3f1e09798815054579e692a54e6c60d6c36edde
AnneMusau <amusau4@gmail.com>
2023-04-27 11:01:56 -0700

    8-print_base16.c and 9-print_comb.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 succes
+ */
+int main(void)
+{
+char c;
+int b;
+c = 'a';
+b = 0;
+while
+(b < 10) 
+{
+putchar(b + '0');
+b++;
+}
+while (c <= 'f') 
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 success
+ *
  */
 int main(void)
 {
-int c;
-
-c = 0;
-while (c < 10){
-putchar(c + '0');
-c++;
+int i;
+for(i = 0;i < 10;i++)
+{
+putchar(i + '0');
+if(i < 9)
+{
+putchar(',');
+putchar(' ');
+}
 }
 putchar('\n');
+getchar();
 return (0);
 }
