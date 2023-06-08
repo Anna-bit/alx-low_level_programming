644e265e1c4ec3a8e938e624bf215164917b39c9
Anne Musau <amusau4@gmail.com>
2023-06-08 14:34:31 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - function that prints the binary representation of no
+ * @n: no to print
+ */
+
+void print_binary(unsigned long int n)
+{
+	int x, count = 0;
+	unsigned long int value;
+
+	for (x = 63; x >= 0; x--)
+	{
+		value = n >> x;
+
+		if (value & 1)
+		{
+			_putchar('1');
+			value++;
+		}
+		else if (value)
+			_putchar('0');
+	}
+	if (!value)
+		_putchar('0');
+}
